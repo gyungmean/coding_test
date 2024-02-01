dd12e1f51250bb150baa3a43588bc70f109fd2ab
Gyungmin Kim <70059000+gyungmean@users.noreply.github.com>
2024-02-01 11:22:15 +0900

    [Silver IV] Title: 카드2, Time: 180 ms, Memory: 45524 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Main {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		// 입력 : N - 카드 숫자
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		Queue<Integer> q = new LinkedList<>();
+		
+		for(int i = 1; i <= N; i++) {
+			q.add(i);
+		}
+		while(q.size() != 1) {
+			q.poll();
+			q.add(q.poll());
+		}
+		System.out.println(q.peek());
+		br.close();
+	}
+
+}
