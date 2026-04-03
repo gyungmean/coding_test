fb9f2a505d5b9a398ba012d18dc6ef00401c7c07
Gyungmin Kim <70059000+gyungmean@users.noreply.github.com>
2026-04-03 10:17:31 +0900

    [level 2] Title: 모음 사전, Time: 0.08 ms, Memory: 97.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int solution(String word) {
+        int answer = 0;
+        
+        int[] weight = {781, 156, 31, 6, 1};
+        char[] vowels = {'A', 'E', 'I', 'O', 'U'};
+        
+        for(int i = 0; i < word.length(); i++) {
+            for(int j = 0; j < 5; j++) {
+                if(word.charAt(i) == vowels[j]) {
+                    answer += j * weight[i] + 1;
+                    break;
+                }
+            }
+        }
+        
+        return answer;
+    }
+}
